43c3a06ca735bef6cd0a419dd6d4315d45df18fd
SingWang123 <gnissingtest@gmail.com>
2024-05-08 16:25:03 +0800

    task 1~4 update

--- week6/main.py
+++ week6/main.py
@@ -50,6 +50,12 @@ for x in myresult:
    print(x)
 
 
+
+# if ('456','456') in myresult:
+#     print(True)
+# else:   
+#     print(False)
+
 # 設定 SessionMiddleware
 app.add_middleware(SessionMiddleware, secret_key = "my_secret_key")
 
@@ -74,26 +80,34 @@ async def signup(request:Request, name: str = Form(None), username_signup: str =
     mycursor.execute(sql_search_username)
     result_username = mycursor.fetchall()
     if (username_signup,) in result_username:
-        return "帳號已存在"
+        return RedirectResponse(url = "/error?error_type=帳號已存在", status_code = 303)
     else:
-        sql = "insert into member (name, username, password) values (%s, %s, %s)"
+        sql = "INSERT INTO member (name, username, password) VALUES (%s, %s, %s)"
         val = (name,username_signup,password_signup)
         mycursor.execute(sql,val)
         mydb.commit()
-        return "註冊成功"
+        return RedirectResponse(url = "/", status_code = 303)
 
 # Vertification Endpoint
 @app.post("/signin")
 async def login(request:Request, username_signin: str = Form(None), password_signin: str = Form(None)):
     # form()內必須要有None，預設值為None表示允許空值送出；若沒有加None會報錯422
-    if not username_signin or not password_signin:
-        return RedirectResponse(url = "/error?error_type=請輸入帳號和密碼", status_code = 303)
-    #帳密都是test，轉向成功頁面
-    elif username_signin == "test" and  password_signin == "test": 
-        request.session["SIGNED-IN"] = True 
+    # 檢查帳號和密碼是否和資料庫一致
+    sql_login = "SELECT username, password FROM member"
+    mycursor.execute(sql_login)
+    result_login = mycursor.fetchall()
+    if (username_signin, password_signin) in result_login:
+        #撈取資料庫該位用戶id,name,username加入session
+        sql_memberdata = "SELECT id,username,name FROM member WHERE username = %s"
+        val_memberdata = (username_signin,)
+        mycursor.execute(sql_memberdata,val_memberdata)
+        memberdata = mycursor.fetchall()
+        request.session["USER-STATE"] = [memberdata[0][0],memberdata[0][1],memberdata[0][2]]
+        request.session["SIGNED-IN"] = True
         return RedirectResponse(url = "/member", status_code = 303) #RedirectResponse會用post的方式取資料，但/member 是get，需加上303狀態才不會出現505錯誤。
+    #帳密不正確，轉到錯誤頁
     else:
-        return RedirectResponse(url = "/error?error_type=帳號或密碼錯誤", status_code = 303)
+        return RedirectResponse(url = "/error?error_type=帳號或密碼輸入錯誤", status_code = 303)
 
 # Success Page
 @app.get("/member", response_class = HTMLResponse)
@@ -101,7 +115,10 @@ async def login(request: Request):
     if "SIGNED-IN" not in request.session or request.session["SIGNED-IN"] == False :
         return RedirectResponse(url = "/")
     else:  
-        return templates.TemplateResponse("member.html", {"request": request})
+        # 從session中獲取使用者名稱
+        user_state = request.session.get("USER-STATE")
+        login_name = user_state[2]
+        return templates.TemplateResponse("member.html", {"request": request, "login_name":login_name})
 
 # Error Page
 @app.get("/error", response_class = HTMLResponse)
@@ -111,7 +128,8 @@ async def member(request: Request, error_type):
 # Signout Endpoint
 @app.get("/signout", response_class = HTMLResponse)
 async def signout(request: Request):
-    request.session["SIGNED-IN"] = False 
+    request.session["SIGNED-IN"] = False
+    del request.session["USER-STATE"] 
     return RedirectResponse(url = "/")
 
 
